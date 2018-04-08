7a170e6d3fb851d46f3ad9be941dc7606685e790
Nate McMaster <nate.mcmaster@gmail.com>
2018-04-08 16:14:48 -0700

    Remove strong name and make types internal

--- src/McMaster.AspNetCore.LetsEncrypt/Internal/ICertificateStore.cs
+++ src/McMaster.AspNetCore.LetsEncrypt/Internal/ICertificateStore.cs
@@ -2,7 +2,7 @@ using System.Security.Cryptography.X509Certificates;
 
 namespace McMaster.AspNetCore.LetsEncrypt.Internal
 {
-    public interface ICertificateStore
+    internal interface ICertificateStore
     {
         X509Certificate2 GetCertificate(string hostName);
         void Save(string hostName, X509Certificate2 certificate);
