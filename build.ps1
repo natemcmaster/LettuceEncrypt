7c5cac285178de286e309b157fbd4104fb82b979
Nate McMaster <nate.mcmaster@gmail.com>
2019-07-30 21:19:53 -0700

    Add Azure Pipelines for CI

--- build.ps1
+++ build.ps1
@@ -3,8 +3,10 @@
 param(
     [ValidateSet('Debug', 'Release')]
     $Configuration = $null,
-	[switch]
-	$IsOfficialBuild,
+    [switch]
+    $ci,
+    [switch]
+    $sign,
     [Parameter(ValueFromRemainingArguments = $true)]
     [string[]]$MSBuildArgs
 )
@@ -12,27 +14,55 @@ param(
 Set-StrictMode -Version 1
 $ErrorActionPreference = 'Stop'
 
-function exec([string]$_cmd) {
-    write-host -ForegroundColor DarkGray ">>> $_cmd $args"
-    $ErrorActionPreference = 'Continue'
-    & $_cmd @args
-    $ErrorActionPreference = 'Stop'
-    if ($LASTEXITCODE -ne 0) {
-        write-error "Failed with exit code $LASTEXITCODE"
-        exit 1
-    }
-}
+Import-Module -Force -Scope Local "$PSScriptRoot/src/common.psm1"
 
 #
 # Main
 #
 
-if (!$Configuration) {
-    $Configuration = if ($env:CI -or $IsOfficialBuild) { 'Release' } else { 'Debug' }
+if ($env:CI -eq 'true') {
+    $ci = $true
 }
 
-if ($IsOfficialBuild) {
-	$MSBuildArgs += '-p:CI=true'
+if (!$Configuration) {
+    $Configuration = if ($ci) { 'Release' } else { 'Debug' }
+}
+
+if ($ci) {
+    $MSBuildArgs += '-p:CI=true'
+}
+
+$isPr = $env:APPVEYOR_PULL_REQUEST_HEAD_COMMIT -or ($env:BUILD_REASON -eq 'PullRequest')
+if (-not (Test-Path variable:\IsCoreCLR)) {
+    $IsWindows = $true
+}
+
+$CodeSign = $sign -or ($ci -and -not $isPr -and $IsWindows)
+if ($CodeSign) {
+    $toolsDir = "$PSScriptRoot/.build/tools"
+    $AzureSignToolPath = "$toolsDir/azuresigntool"
+    if ($IsWindows) {
+        $AzureSignToolPath += ".exe"
+    }
+
+    if (-not (Test-Path $AzureSignToolPath)) {
+        exec dotnet tool install --tool-path $toolsDir `
+        AzureSignTool `
+        --version 2.0.17
+    }
+
+    $nstDir = "$toolsDir/nugetsigntool/1.1.4"
+    $NuGetKeyVaultSignToolPath = "$nstDir/tools/net471/NuGetKeyVaultSignTool.exe"
+    if (-not (Test-Path $NuGetKeyVaultSignToolPath)) {
+        New-Item $nstDir -ItemType Directory -ErrorAction Ignore | Out-Null
+        Invoke-WebRequest https://github.com/onovotny/NuGetKeyVaultSignTool/releases/download/v1.1.4/NuGetKeyVaultSignTool.1.1.4.nupkg `
+            -OutFile "$nstDir/NuGetKeyVaultSignTool.zip"
+        Expand-Archive "$nstDir/NuGetKeyVaultSignTool.zip" -DestinationPath $nstDir
+    }
+
+    $MSBuildArgs += '-p:CodeSign=true'
+    $MSBuildArgs += "-p:AzureSignToolPath=$AzureSignToolPath"
+    $MSBuildArgs += "-p:NuGetKeyVaultSignToolPath=$NuGetKeyVaultSignToolPath"
 }
 
 $artifacts = "$PSScriptRoot/artifacts/"
@@ -41,5 +71,15 @@ Remove-Item -Recurse $artifacts -ErrorAction Ignore
 
 exec dotnet build --configuration $Configuration '-warnaserror:CS1591' @MSBuildArgs
 exec dotnet pack --no-restore --no-build --configuration $Configuration -o $artifacts @MSBuildArgs
+
+# [string[]] $testArgs=@()
+# if ($env:TF_BUILD) {
+#     $testArgs += '--logger', 'trx'
+# }
+
+# exec dotnet test --no-restore --no-build --configuration $Configuration '-clp:Summary' `
+#     "$PSScriptRoot/test/Plugins.Tests/McMaster.NETCore.Plugins.Tests.csproj" `
+#     @testArgs `
+#     @MSBuildArgs
 
 write-host -f magenta 'Done'
