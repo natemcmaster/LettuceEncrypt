13380e32ce66eb6787ad18dea582b953c7e3de8c
Nate McMaster <nate.mcmaster@gmail.com>
2020-01-05 17:39:43 -0800

    fixup! Add more tests

--- test/LetsEncrypt.UnitTests/HttpChallengeResponseMiddlewareTests.cs
+++ test/LetsEncrypt.UnitTests/HttpChallengeResponseMiddlewareTests.cs
@@ -4,6 +4,8 @@ using McMaster.AspNetCore.LetsEncrypt.Internal;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Moq;
 using Xunit;
 
 #if NETCOREAPP2_1
@@ -15,25 +17,28 @@ namespace LetsEncrypt.UnitTests
     public class HttpChallengeResponseMiddlewareTests
     {
         [Fact]
-        public async Task ItResponsesWithToken()
+        public async Task ItRespondsWithTokenValue()
         {
             var services = new ServiceCollection()
                 .AddLogging()
+                .AddScoped<IMiddlewareFactory, MiddlewareFactory>()
                 .AddLetsEncrypt()
                 .Services
                 .BuildServiceProvider(validateScopes: true);
 
             var appBuilder = new ApplicationBuilder(services);
             appBuilder.UseLetsEncryptDomainVerification();
-            appBuilder.Run(_ => Task.CompletedTask);
 
             var app = appBuilder.Build();
 
             var challengeStore = services.GetRequiredService<IHttpChallengeResponseStore>();
-            challengeStore.AddChallengeResponse("TOKEN-1", "Hello World");
+            const string TokenValue = "abcxyz123";
+            challengeStore.AddChallengeResponse("TOKEN-1", TokenValue);
 
+            using var scope = services.CreateScope();
             var context = new DefaultHttpContext
             {
+                RequestServices = scope.ServiceProvider,
                 Request =
                 {
                     Path = "/.well-known/acme-challenge/TOKEN-1",
@@ -50,7 +55,49 @@ namespace LetsEncrypt.UnitTests
             var reader = new StreamReader(context.Response.Body);
             var streamText = reader.ReadToEnd();
 
-            Assert.Equal("Hello World", streamText);
+            Assert.Equal(TokenValue, streamText);
+            Assert.Equal("application/octet-stream", context.Response.ContentType);
+            Assert.Equal(TokenValue.Length, context.Response.ContentLength);
+        }
+
+        [Fact]
+        public async Task ItForwardsToNextMiddlwareForUnrecognizedChallenge()
+        {
+            var servicesCollection = new ServiceCollection()
+                .AddLogging()
+                .AddScoped<IMiddlewareFactory, MiddlewareFactory>()
+                .AddLetsEncrypt()
+                .Services;
+
+            var mockChallenge = new Mock<IHttpChallengeResponseStore>();
+            mockChallenge
+                .Setup(s => s.TryGetResponse("unknown", out It.Ref<string>.IsAny))
+                .Returns(false)
+                .Verifiable();
+
+            servicesCollection.Replace(ServiceDescriptor.Singleton(mockChallenge.Object));
+
+            var services = servicesCollection.BuildServiceProvider(validateScopes: true);
+
+            var appBuilder = new ApplicationBuilder(services);
+            appBuilder.UseLetsEncryptDomainVerification();
+
+            var app = appBuilder.Build();
+
+            using var scope = services.CreateScope();
+            var context = new DefaultHttpContext
+            {
+                RequestServices = scope.ServiceProvider,
+                Request =
+                {
+                    Path = "/.well-known/acme-challenge/unknown",
+                },
+            };
+
+            await app.Invoke(context);
+
+            Assert.Equal(StatusCodes.Status404NotFound, context.Response.StatusCode);
+            mockChallenge.VerifyAll();
         }
     }
 }
