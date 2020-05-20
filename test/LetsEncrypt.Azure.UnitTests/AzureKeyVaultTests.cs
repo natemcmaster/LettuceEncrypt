ff2bcc423cdd76c9ce0bf5ff7c2ec934502f4cb0
Samuel Zayas <s.zayas@gmail.com>
2020-05-20 16:22:57 +0200

    Removed private certificate generation, use testutils certificate generation, don't use mock when not needed, use null logger.

--- test/LetsEncrypt.Azure.UnitTests/AzureKeyVaultTests.cs
+++ test/LetsEncrypt.Azure.UnitTests/AzureKeyVaultTests.cs
@@ -1,18 +1,14 @@
-﻿using System;
-using System.Linq;
-using System.Net;
-using System.Security.Cryptography;
-using System.Security.Cryptography.X509Certificates;
+﻿using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Azure.Security.KeyVault.Certificates;
 using Azure.Security.KeyVault.Secrets;
 using McMaster.AspNetCore.LetsEncrypt;
+using LetsEncrypt.UnitTests;
 using Microsoft.Extensions.DependencyInjection;
-//using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Hosting.Internal;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Options;
 using Moq;
 using Xunit;
@@ -25,27 +21,6 @@ namespace LetsEncrypt.Azure.UnitTests
 {
     public class AzureKeyVaultTests
     {
-        private X509Certificate2 BuildSelfSignedServerCertificate(string certificateHost)
-        {
-            var CN = certificateHost.Replace("https://", ""); // Sanitize from certificate common name, dns name and friendly name
-
-            var sanBuilder = new SubjectAlternativeNameBuilder();
-            sanBuilder.AddIpAddress(IPAddress.Loopback);
-            sanBuilder.AddIpAddress(IPAddress.IPv6Loopback);
-            sanBuilder.AddDnsName(CN);
-            sanBuilder.AddDnsName($"www.{CN}");
-
-            var distinguishedName = new X500DistinguishedName($"CN={CN}");
-
-            using var rsa = RSA.Create(2048);
-
-            var request = new CertificateRequest(distinguishedName, rsa, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
-
-            var certificate = request.CreateSelfSigned(new DateTimeOffset(DateTime.UtcNow.AddDays(-1)), new DateTimeOffset(DateTime.UtcNow.AddDays(1)));
-            var exportedCertficiate = new X509Certificate2(certificate.Export(X509ContentType.Pfx, "p@ssw0rd"), "p@ssw0rd", X509KeyStorageFlags.Exportable);
-            return exportedCertficiate;
-        }
-
         private static void DefaultConfigure(AzureKeyVaultCertificateRepositoryOptions options)
         {
             options.AzureKeyVaultEndpoint = "http://something";
@@ -95,18 +70,14 @@ namespace LetsEncrypt.Azure.UnitTests
 
             var certclient = new Mock<CertificateClient>();
             var secretclient = new Mock<SecretClient>();
-            var logger = new Mock<ILogger<AzureKeyVaultCertificateRepository>>();
-            var options = new Mock<IOptions<LetsEncryptOptions>>();
+            var options = Options.Create(new LetsEncryptOptions());
 
-            options.Setup(o => o.Value).Returns(new LetsEncryptOptions
+            options.Value.DomainNames = new[] { Domain1, Domain2 }; 
+            
+            var repository = new AzureKeyVaultCertificateRepository(certclient.Object, secretclient.Object, options, NullLogger<AzureKeyVaultCertificateRepository>.Instance);
+            foreach(var domain in options.Value.DomainNames)
             {
-                DomainNames = new[] { Domain1, Domain2 }
-            });
-
-            var repository = new AzureKeyVaultCertificateRepository(certclient.Object, secretclient.Object, options.Object, logger.Object);
-            foreach(var domain in options.Object.Value.DomainNames)
-            {
-                var certificateToSave = BuildSelfSignedServerCertificate(domain);
+                var certificateToSave = TestUtils.CreateTestCert(domain);
                 await repository.SaveAsync(certificateToSave, CancellationToken.None);
             }
 
@@ -123,15 +94,11 @@ namespace LetsEncrypt.Azure.UnitTests
 
             var certclient = new Mock<CertificateClient>();
             var secretclient = new Mock<SecretClient>();
-            var logger = new Mock<ILogger<AzureKeyVaultCertificateRepository>>();
-            var options = new Mock<IOptions<LetsEncryptOptions>>();
+            var options = Options.Create(new LetsEncryptOptions());
 
-            options.Setup(o => o.Value).Returns(new LetsEncryptOptions
-            {
-                DomainNames = new[] { Domain1, Domain2 }
-            });
+            options.Value.DomainNames = new[] { Domain1, Domain2 };
 
-            var repository = new AzureKeyVaultCertificateRepository(certclient.Object, secretclient.Object, options.Object, logger.Object);
+            var repository = new AzureKeyVaultCertificateRepository(certclient.Object, secretclient.Object, options, NullLogger<AzureKeyVaultCertificateRepository>.Instance);
 
             var certificates = await repository.GetCertificatesAsync(CancellationToken.None);
 
