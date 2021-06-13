7728a8909887511996e106b1d385c99430e70b13
Nate McMaster <nate.mcmaster@gmail.com>
2021-06-12 22:06:15 -0700

    refactor: if multiple validation methods are enabled, run serially

---
+++ src/LettuceEncrypt/Internal/Http01DomainValidator.cs
@@ -0,0 +1,62 @@
+﻿// Copyright (c) Nate McMaster.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Certes.Acme;
+using Certes.Acme.Resource;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+#if NETSTANDARD2_0
+using IHostApplicationLifetime = Microsoft.Extensions.Hosting.IApplicationLifetime;
+#endif
+
+namespace LettuceEncrypt.Internal
+{
+    internal class Http01DomainValidator : DomainOwnershipValidator
+    {
+        private readonly IHttpChallengeResponseStore _challengeStore;
+
+        public Http01DomainValidator(IHttpChallengeResponseStore challengeStore,
+            IHostApplicationLifetime appLifetime,
+            AcmeClient client, ILogger logger, string domainName) : base(appLifetime, client, logger, domainName)
+        {
+            _challengeStore = challengeStore;
+        }
+
+        public override async Task ValidateOwnershipAsync(IAuthorizationContext authzContext, CancellationToken cancellationToken)
+        {
+            await PrepareHttpChallengeResponseAsync(authzContext, cancellationToken);
+            await ValidateOwnershipAsync(authzContext, cancellationToken);
+        }
+
+        private async Task PrepareHttpChallengeResponseAsync(
+            IAuthorizationContext authorizationContext,
+            CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            if (_client == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var httpChallenge = await _client.CreateChallengeAsync(authorizationContext, ChallengeTypes.Http01);
+            if (httpChallenge == null)
+            {
+                throw new InvalidOperationException(
+                    $"Did not receive challenge information for challenge type {ChallengeTypes.Http01}");
+            }
+
+            var keyAuth = httpChallenge.KeyAuthz;
+            _challengeStore.AddChallengeResponse(httpChallenge.Token, keyAuth);
+
+            _logger.LogTrace("Waiting for server to start accepting HTTP requests");
+            await _appStarted.Task;
+
+            _logger.LogTrace("Requesting server to validate HTTP challenge");
+            await _client.ValidateChallengeAsync(httpChallenge);
+        }
+    }
+}
---
+++ src/LettuceEncrypt/Internal/TlsAlpn01DomainValidator.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) Nate McMaster.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Certes.Acme;
+using Certes.Acme.Resource;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+#if NETSTANDARD2_0
+using IHostApplicationLifetime = Microsoft.Extensions.Hosting.IApplicationLifetime;
+#endif
+
+namespace LettuceEncrypt.Internal
+{
+    internal class TlsAlpn01DomainValidator : DomainOwnershipValidator
+    {
+        private readonly TlsAlpnChallengeResponder _tlsAlpnChallengeResponder;
+
+        public TlsAlpn01DomainValidator(TlsAlpnChallengeResponder tlsAlpnChallengeResponder,
+            IHostApplicationLifetime appLifetime,
+            AcmeClient client, ILogger logger, string domainName) : base(appLifetime, client, logger, domainName)
+        {
+            _tlsAlpnChallengeResponder = tlsAlpnChallengeResponder;
+        }
+
+        public override async Task ValidateOwnershipAsync(IAuthorizationContext authzContext, CancellationToken cancellationToken)
+        {
+            try
+            {
+                await PrepareTlsAlpnChallengeResponseAsync(authzContext, _domainName, cancellationToken);
+                await ValidateOwnershipAsync(authzContext, cancellationToken);
+            }
+            finally
+            {
+                // cleanup after authorization is done to skip unnecessary cert lookup on all incoming SSL connections
+                _tlsAlpnChallengeResponder.DiscardChallenge(_domainName);
+            }
+        }
+
+        private async Task PrepareTlsAlpnChallengeResponseAsync(
+            IAuthorizationContext authorizationContext,
+            string domainName,
+            CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            var tlsAlpnChallenge = await _client.CreateChallengeAsync(authorizationContext, ChallengeTypes.TlsAlpn01);
+
+            _tlsAlpnChallengeResponder.PrepareChallengeCert(domainName, tlsAlpnChallenge.KeyAuthz);
+
+            _logger.LogTrace("Waiting for server to start accepting HTTP requests");
+            await _appStarted.Task;
+
+            _logger.LogTrace("Requesting server to validate TLS/ALPN challenge");
+            await _client.ValidateChallengeAsync(tlsAlpnChallenge);
+        }
+    }
+}
